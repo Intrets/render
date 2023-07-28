1d5241f75a17252ee071d316ced6de9d5af94fa2
Intrets <geen.inspiratie824@gmail.com>
2023-07-28 02:07:52 +0200

    CachedValues, like CachedValue but stores multiple indices

--- include/render/CachedValue.h
+++ include/render/CachedValue.h
@@ -20,6 +20,8 @@ namespace render
 			using type = ValueType;
 			using base = Info;
 		};
+
+		inline static bool const dummyValue = false;
 	}
 
 	template<auto ptr, class StorageType = typename detail::template AccessesValue<ptr>::type, class I = typename detail::template AccessesValue<ptr>::base>
@@ -30,8 +32,7 @@ namespace render
 
 		StorageType value{};
 
-		inline static bool dummyValue = false;
-		bool* valid = &dummyValue;
+		bool const* valid = &detail::dummyValue;
 		render::RenderInfoBase<Info>* info = nullptr;
 
 		int index = 0;
@@ -111,6 +112,109 @@ namespace render
 		}
 	};
 
+	template<auto ptr, class StorageType = typename detail::template AccessesValue<ptr>::type, class I = typename detail::template AccessesValue<ptr>::base>
+	struct CachedValues
+	{
+		using ValueType = typename detail::template AccessesValue<ptr>::type;
+		using Info = typename detail::template AccessesValue<ptr>::base;
+
+		StorageType value{};
+
+		bool const* valid = &detail::dummyValue;
+		render::RenderInfoBase<Info>* info = nullptr;
+
+		std::vector<int> indices{};
+
+		StorageType& get() {
+			return this->value;
+		}
+
+		StorageType const& get() const {
+			return this->value;
+		}
+
+		operator StorageType() {
+			return this->value;
+		}
+
+		operator StorageType const() const {
+			return this->value;
+		}
+
+		StorageType* operator->() {
+			return &this->value;
+		}
+
+		StorageType const* operator->() const {
+			return &this->value;
+		}
+
+		void clear(StorageType const& v){
+			this->indices.clear();
+			this->valid = &detail::dummyValue;
+			this->info = nullptr;
+			this->value = v;
+		}
+
+		CachedValues() = default;
+		~CachedValues() = default;
+
+		template<class... Args>
+		CachedValues(Args&&... args)
+		    : value(std::forward<Args>(args)...) {
+		}
+
+		CachedValues& operator=(StorageType const& v) {
+			this->value = v;
+
+			return *this;
+		}
+
+		CachedValues& operator=(StorageType&& v) {
+			this->value = std::move(v);
+
+			return *this;
+		}
+
+		DEFAULT_COPY_MOVE(CachedValues);
+
+		bool isValid() const {
+			return *this->valid;
+		}
+
+		void send() {
+			if (this->isValid()) {
+				if constexpr (std::same_as<te::angle<float>, StorageType>) {
+					for (auto index : this->indices) {
+						this->info->data[index].*ptr = this->value.get();
+					}
+				}
+				else {
+					for (auto index : this->indices) {
+						this->info->data[index].*ptr = this->value;
+					}
+				}
+			}
+		}
+
+		void send(StorageType s) {
+			if (this->value != s && this->isValid()) {
+				if constexpr (std::same_as<te::angle<float>, StorageType>) {
+					for (auto index : this->indices) {
+						this->info->data[index].*ptr = s.get();
+					}
+				}
+				else {
+					for (auto index : this->indices) {
+						this->info->data[index].*ptr = s;
+					}
+				}
+			}
+
+			this->value = s;
+		}
+	};
+
 	template<class Info>
 	struct CachedValueInitializer
 	{
@@ -123,6 +227,18 @@ namespace render
 			value.info = &this->info;
 			value.valid = this->valid;
 			value.index = this->index;
+
+			return *this;
+		}
+
+		template<auto ptr, class T>
+		CachedValueInitializer<Info>& init(CachedValues<ptr, T>& value) {
+			assert(value.info == nullptr || value.info == &this->info);
+			value.info = &this->info;
+			assert(value.valid == &detail::dummyValue || value.valid == this->valid);
+			value.valid = this->valid;
+
+			value.indices.push_back(this->index);
 
 			return *this;
 		}
